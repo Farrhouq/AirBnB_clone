d98b5aa60a5786a5f6eaaf98dc03a1a81ed51f14
Farrhouq <farouqimoro@gmail.com>
2024-03-24 16:09:37 +0000

    update

---
+++ console.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module contains the entry point for the console"""
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """The console command interpreter class"""
+
+    
+
