b507b8090db4a3ae02cb135e7ca107bd3484c977
Farrhouq <farouqimoro@gmail.com>
2024-03-24 13:46:49 +0000

    update

--- models/base_model.py
+++ models/base_model.py
@@ -9,10 +9,22 @@ import datetime
 class BaseModel:
     """defines all common attributes/methods for other classes"""
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+    # def __init__(self):
+    #     self.id = str(uuid.uuid4())
+    #     self.created_at = datetime.datetime.now()
+    #     self.updated_at = datetime.datetime.now()
+
+    def __init__(self, *args, **kwargs):
+        """instantiating a new model with the given attributes"""
+        
+        if kwargs:
+            for key, val in kwargs.items():
+                if key not in  ["__class__",  "updated_at", "created_at"]:
+                    self.key = val
+            self.updated_at = datetime.datetime.fromisoformat(kwargs['updated_at'])
+            self.created_at = datetime.datetime.fromisoformat(kwargs['created_at'])
+
+
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
