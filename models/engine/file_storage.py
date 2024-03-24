ccfd004f041a4ef48445c0e8074414a8d6e74ae5
Farrhouq <farouqimoro@gmail.com>
2024-03-24 14:43:00 +0000

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python3
-"""Contains a a class FileStorage that serializes instances
-to a JSON file and deserializes JSON file to instances"""
+"""This is the module for the storage class"""
 
-import json
+import os
+
 
 class FileStorage:
+    """This is the file storage class"""
+
     __file_path = "file.json"
     __objects = {}
 
     def __init__(self):
+        """This is the constructor of the FileStorage class"""
         pass
 
     def all(self):
-        """return a list of objects"""
+        """This is the function used to access the __objects"""
         return FileStorage.__objects
-    
-    def new(self, obj):
-        """save an object"""
 
+    def new(self, obj):
+        """adds the obj in the __objects"""
         if obj:
-            FileStorage.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
+            key = f"{obj.__class__.__name__}.{obj.id}"
+            FileStorage.__objects[key] = obj
 
     def save(self):
         """Serializes the __objects and save it to the file"""
@@ -32,11 +35,38 @@ class FileStorage:
             json_string = json.JSONEncoder().encode(json_dict)
             file.write(json_string)
 
-
     def reload(self):
-        """reload all objects only if the file exists"""
-        try:
-            with open(FileStorage.__file_path, "r") as f:
-                FileStorage.__objects = json.load(f)
-        except Exception as e:
-            pass
+        """Reloads the json string form the file to __objects"""
+        import json
+        from ..base_model import BaseModel
+        from ..user import User
+        from ..state import State
+        from ..city import City
+        from ..amenity import Amenity
+        from ..place import Place
+        from ..review import Review
+        if os.path.isfile(FileStorage.__file_path):
+            with open(FileStorage.__file_path, "r") as file:
+                json_string = file.read()
+                if len(json_string) > 0:
+                    json_dict = json.JSONDecoder().decode(json_string)
+                    for k, v in json_dict.items():
+                        name = k.split(".")
+                        FileStorage.__objects[k] = eval(
+                                "{}(**v)".format(name[0]))
+
+    def delete(self, key):
+        """deletes the object with the specified id"""
+        if key:
+            for k in FileStorage.__objects.keys():
+                if k == key:
+                    del FileStorage.__objects[k]
+                    break
+
+    def update(self, key, attr, value):
+        """The update function to update an object"""
+        if key and attr and value:
+            for k in FileStorage.__objects.keys():
+                if key == k:
+                    FileStorage.__objects[k].__dict__[attr] = value
+                    break
