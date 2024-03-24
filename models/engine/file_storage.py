771aabe4c6f20723ac60d019abb06cd719cf8bcb
Farrhouq <farouqimoro@gmail.com>
2024-03-24 14:52:32 +0000

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -27,7 +27,6 @@ class FileStorage:
         with open(FileStorage.__file_path, "w", encoding="utf-8") as file:
             json_dict = {}
             for k, v in FileStorage.__objects.items():
-                print(k, v)
                 if v and k:
                     json_dict[k] = v.to_dict()
             json_string = json.JSONEncoder().encode(json_dict)
