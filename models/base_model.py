b8a080fe79a4d429a9b78411b4b95a826db88cd5
Farrhouq <farouqimoro@gmail.com>
2024-03-24 14:43:57 +0000

    update

--- models/base_model.py
+++ models/base_model.py
@@ -1,46 +1,45 @@
 #!/usr/bin/python3
-""" Contains a class that defines all common attributes/methods for
-other classes"""
+"""This is the BaseModel class"""
 
 import uuid
-import datetime
-from .__init__ import storage
+from datetime import datetime
+from models import storage
+
 
 class BaseModel:
-    """defines all common attributes/methods for other classes"""
-
-    # def __init__(self):
-    #     self.id = str(uuid.uuid4())
-    #     self.created_at = datetime.datetime.now()
-    #     self.updated_at = datetime.datetime.now()
+    """This is the BaseModel class"""
 
     def __init__(self, *args, **kwargs):
-        """instantiating a new model with the given attributes"""
+        """This is the instantiotor method"""
 
         if kwargs:
-            self.id = kwargs['id']
-            self.updated_at = datetime.datetime.fromisoformat(kwargs['updated_at'])
-            self.created_at = datetime.datetime.fromisoformat(kwargs['created_at'])
+            for k, v in kwargs.items():
+                if not k.startswith("__"):
+                    if k == "created_at":
+                        self.__dict__[k] = datetime.fromisoformat(v)
+                    elif k == "updated_at":
+                        self.__dict__[k] = datetime.fromisoformat(v)
+                    else:
+                        self.__dict__[k] = v
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             storage.new(self)
 
-
-
     def __str__(self):
+        """The string representation of the object"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """called when the object is saved"""
-        self.updated_at = datetime.datetime.now()
+        """This is the save funtion that saves and updates the update time"""
+        self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
-        """to dict"""
-        obj_dict = dict(self.__dict__)
-        obj_dict["__class__"] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
+        """Returns a dictionary representation of the calling object"""
+        ret = dict(self.__dict__)
+        ret["__class__"] = self.__class__.__name__
+        ret["created_at"] = self.created_at.isoformat()
+        ret["updated_at"] = self.updated_at.isoformat()
+        return ret
