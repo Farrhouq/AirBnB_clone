db36a5cf44f760aad79d69bb705aa693e3e8878f
Farrhouq <farouqimoro@gmail.com>
2024-03-24 12:33:04 +0000

    update

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,9 @@
-""" Contains a class that defines all common attributes/methods for other classes"""
+""" Contains a class that defines all common attributes/methods for
+other classes"""
 
 import uuid
 import datetime
+
 
 class BaseModel:
     """defines all common attributes/methods for other classes"""
@@ -13,17 +15,17 @@ class BaseModel:
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__} "
-    
 
     def save(self):
         """called when the object is saved"""
         self.updated_at = datetime.datetime.now()
 
-
     def to_dict(self):
         """to dict"""
         obj_dict = self.__dict__
         obj_dict["__class__"] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat("%Y-%m-%dT%H:%M:%S.%f")
-        obj_dict['updated_at'] = self.updated_at.isoformat("%Y-%m-%dT%H:%M:%S.%f")
+        obj_dict['created_at'] = self.created_at.isoformat(
+            "%Y-%m-%dT%H:%M:%S.%f")
+        obj_dict['updated_at'] = self.updated_at.isoformat(
+            "%Y-%m-%dT%H:%M:%S.%f")
         return obj_dict
