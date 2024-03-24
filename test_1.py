9549172f65f989b549b2e42704227d06fcbf4624
Farrhouq <farouqimoro@gmail.com>
2024-03-24 13:34:39 +0000

    update

--- test_1.py
+++ test_1.py
@@ -1,12 +1,19 @@
+"""This module contains tests for the BaseModel class"""
+
 import unittest
 from models.base_model import BaseModel
 
 
 class TestModel(unittest.TestCase):
+    """testing the base model"""
+
     def setUp(self):
+        """set public properties"""
+
         self.model = BaseModel()
 
     def test_to_dict(self):
+        """testing the to_dict method"""
         self.assertTrue(self.model.to_dict() != {""})
 
 
